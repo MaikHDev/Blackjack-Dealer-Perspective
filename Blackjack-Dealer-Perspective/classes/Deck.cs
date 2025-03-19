b03c3f29d9d4723baf08235a18d6c59c4bc10f6a
Maik Hendriks <maikew2006@gmail.com>
2025-03-19 11:53:22 +0100

    # Deck + Added Deck class with a list consisting of unique cards.

---
+++ Blackjack-Dealer-Perspective/classes/Deck.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack_Dealer_Perspective.classes
+{
+    internal class Deck
+    {
+        public readonly List<Card> Cards = new List<Card>();
+
+        public Deck()
+        {
+            foreach (Suits suit in Enum.GetValues(typeof(Suits)))
+            {
+                foreach (Ranks rank in Enum.GetValues(typeof(Ranks)))
+                {
+                    Card card = new Card(rank, suit);
+                    Cards.Add(card);
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            string result = "";
+
+            foreach (Card card in Cards)
+            {
+                result += card.ToString();
+                result += "\n";
+            }
+
+            return result;
+        }
+    }
+}
