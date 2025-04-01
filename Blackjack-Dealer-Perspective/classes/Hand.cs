87e75cd878693f87b05b64f9bb4115e8a677b4f5
Maik Hendriks <maikew2006@gmail.com>
2025-04-01 13:27:22 +0200

    # Hand + Added hand which can hold multiple cards. + Bet can't be changed once set, only doubled. + Double down function that returns true on ability to double down. + Split returns bool for ability to split. + AddCardToHand for adding cards to the hand. + Stand to stop playing the hand.

---
+++ Blackjack-Dealer-Perspective/classes/Hand.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack_Dealer_Perspective.classes
+{
+    internal class Hand
+    {
+        public List<Card> Cards { get; private set; } = new List<Card>();
+
+        private Nullable<int> bet = null;
+
+        public int? Bet
+        {
+            get { return bet.GetValueOrDefault(); }
+            set
+            {
+                if (!bet.HasValue)
+                {
+                    bet = value;
+                }
+            }
+        }
+
+        public bool DoubledDown { get; private set; } = false;
+
+        private bool stood = false;
+        public bool HasStood
+        {
+            get { return stood; }
+            set
+            {
+                if (true == stood)
+                {
+                    return;
+                }
+                stood = value;
+            }
+        }
+
+        public void StandHand()
+        {
+            HasStood = true;
+        }
+
+        public bool DoubleDown()
+        {
+            if (true == DoubledDown)
+            {
+                Console.WriteLine("Already doubled the bet of this hand, it is not possible to double again!");
+                return false;
+            }
+
+            this.bet = this.bet * 2;
+            DoubledDown = true;
+            stood = true;
+            return true;
+        }
+        public bool Split()
+        {
+            if (Cards.Count == 2 && Cards[0].GetValue() == Cards[1].GetValue())
+            {
+                return true;
+            }
+
+            Console.WriteLine("Not possible to split this hand!");
+            return false;
+        }
+
+        public void AddCardToHand(Card card)
+        {
+            Cards.Add(card);
+        }
+    }
+}
