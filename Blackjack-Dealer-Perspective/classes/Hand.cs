3aa9c25d35ed067acfde6f25aea09debfef693ca
Maik Hendriks <maikew2006@gmail.com>
2025-04-07 11:28:47 +0200

    Added function to get hand value and calculate the total value based on aces

--- Blackjack-Dealer-Perspective/classes/Hand.cs
+++ Blackjack-Dealer-Perspective/classes/Hand.cs
@@ -15,11 +15,9 @@ namespace Blackjack_Dealer_Perspective.classes
         // Imagebox list --
         public List<PictureBox> HandImgBox = new List<PictureBox>();
 
-        public int? Bet
-        {
+        public int? Bet {
             get { return bet.GetValueOrDefault(); }
-            set
-            {
+            set {
                 if (!bet.HasValue)
                 {
                     bet = value;
@@ -30,11 +28,9 @@ namespace Blackjack_Dealer_Perspective.classes
         public bool DoubledDown { get; private set; } = false;
 
         private bool stood = false;
-        public bool HasStood
-        {
+        public bool HasStood {
             get { return stood; }
-            set
-            {
+            set {
                 if (true == stood)
                 {
                     return;
@@ -43,13 +39,46 @@ namespace Blackjack_Dealer_Perspective.classes
             }
         }
 
-        public void StandHand()
-        {
+        public int? GetHandValue() {
+            if (Cards == null || !Cards.Any())
+            {
+                return null;
+            }
+
+            int handTotalValue = 0;
+            int aceCount = 0;
+
+            foreach (var card in Cards)
+            {
+                var cardValue = card.GetValue();
+                if (cardValue == null)
+                {
+                    continue;
+                }
+
+                handTotalValue += (int)cardValue;
+
+                if (card.Rank == Ranks.ACE)
+                {
+                    aceCount++;
+                }
+            }
+
+            while (handTotalValue > 21 && aceCount > 0)
+            {
+                handTotalValue -= 10;
+                aceCount--;
+            }
+
+            return handTotalValue > 0 ? handTotalValue : null;
+        }
+
+
+        public void StandHand() {
             HasStood = true;
         }
 
-        public bool DoubleDown()
-        {
+        public bool DoubleDown() {
             if (true == DoubledDown)
             {
                 Console.WriteLine("Already doubled the bet of this hand, it is not possible to double again!");
@@ -61,8 +90,7 @@ namespace Blackjack_Dealer_Perspective.classes
             stood = true;
             return true;
         }
-        public bool Split()
-        {
+        public bool Split() {
             if (Cards.Count == 2 && Cards[0].GetValue() == Cards[1].GetValue())
             {
                 return true;
@@ -72,8 +100,7 @@ namespace Blackjack_Dealer_Perspective.classes
             return false;
         }
 
-        public void AddCardToHand(Card card)
-        {
+        public void AddCardToHand(Card card) {
             Cards.Add(card);
         }
     }
