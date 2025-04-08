65514a428576cd45349fcb118903b26e37ca1eb1
Maik Hendriks <maikew2006@gmail.com>
2025-04-08 09:48:04 +0200

    # CardHolder the baseline for dealer and player class

---
+++ Blackjack-Dealer-Perspective/classes/CardHolder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack_Dealer_Perspective.classes
+{
+    abstract class CardHolder
+    {
+        public readonly string Name;
+
+        public CardHolder(string name) {
+            this.Name = name;
+        }
+    }
+}
