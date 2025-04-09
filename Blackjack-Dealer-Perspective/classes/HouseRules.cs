b672766ca771a1508c51dc9cc0668a7537a433c6
Maik Hendriks <123378700+MaikHDev@users.noreply.github.com>
2025-04-09 08:39:31 +0200

    Addes House Rules with some configureable and non configureable variables (#4)

---
+++ Blackjack-Dealer-Perspective/classes/HouseRules.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack_Dealer_Perspective.classes
+{
+    internal class HouseRules
+    {
+        private static HouseRules _instance;
+        private static readonly object _lock = new object();
+
+        // Configureable ->
+        public readonly int PlayerAmount;
+        public readonly int MaxHandSize;
+        public readonly int MinBet;
+        public readonly int ShoeSize; // In decks
+
+        public readonly bool StandOnSoft = true;
+
+        // "Non configureable" -> 
+        public readonly int MaxPlayerAmount = 8;
+        public readonly int DealerStandAmount = 17;
+
+        public static HouseRules GetInstance(int playerAmount = 4, int maxHandSize = 8, int minBet = 10, int shoeSize = 8, bool standOnSoft = true) {
+            if (_instance == null)
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new HouseRules(playerAmount, maxHandSize, minBet, shoeSize, standOnSoft);
+                    }
+                }
+            }
+            return _instance;
+        }
+
+        private HouseRules(int playerAmount, int maxHandSize, int minBet, int shoeSize, bool standOnSoft) {
+            PlayerAmount = (playerAmount <= 0 || playerAmount > MaxPlayerAmount) ? PlayerAmount : playerAmount;
+            MaxHandSize = (maxHandSize <= 0) ? MaxHandSize : maxHandSize;
+            MinBet = (minBet <= 0) ? MinBet : minBet;
+            ShoeSize = (shoeSize <= 0) ? ShoeSize : shoeSize;
+            StandOnSoft = standOnSoft;
+        }
+    }
+}
